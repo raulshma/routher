0ac3a9783ad97c26ac990dd19fedfe2eee9b2764
raulshma <raulshma@gmail.com>
2025-07-21 21:32:44 +0530

    Update package dependencies for improved functionality and performance. Upgraded @react-native-async-storage/async-storage to 2.1.2, expo-location to 18.1.6, react-native-maps to 1.20.1, and react-native-svg to 15.11.2. Removed unused import from _layout.tsx.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated/lib/reanimated-js/Reanimated';
 
 import { TamaguiProvider } from '@tamagui/core';
 import config from '../tamagui.config';
